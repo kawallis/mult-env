c90ea56240ede65d4d88adf2a78aa50a85e4215b
wallaccm425 <wallaccm@icloud.com>
2019-01-15 21:49:18 -0800

    first commit

---
+++ src/graphql/queries.js
@@ -0,0 +1,33 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const getBanner = `query GetBanner {
+  getBanner {
+    title
+    message
+  }
+}
+`;
+export const getTodo = `query GetTodo($id: ID!) {
+  getTodo(id: $id) {
+    id
+    name
+    description
+  }
+}
+`;
+export const listTodos = `query ListTodos(
+  $filter: ModelTodoFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      name
+      description
+    }
+    nextToken
+  }
+}
+`;
